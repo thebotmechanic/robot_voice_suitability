ec4d60f4f7ea035887d97ae99faf1e454ee7b502
Conor McGinn <mcginnc@tcd.ie>
2018-08-23 23:40:53 +0100

    first commit

---
+++ Robot_Voice.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy2 Experiment Builder (v1.90.2),
+    on Thu 23 Aug 2018 12:38:13 IST
+If you publish work using this script please cite the PsychoPy publications:
+    Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
+        Journal of Neuroscience Methods, 162(1-2), 8-13.
+    Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy.
+        Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
+"""
+
+from __future__ import absolute_import, division
+from psychopy import locale_setup, sound, gui, visual, core, data, event, logging, clock
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle
+import os  # handy system and path functions
+import sys  # to get file system encoding
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+expName = 'Robot_Voice'  # from the Builder filename that created this script
+expInfo = {'session': '001', 'participant': ''}
+dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath=None,
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+
+# Start Code - component code to be run before the window creation
+
+# Setup the Window
+win = visual.Window(
+    size=[1440, 810], fullscr=True, screen=0,
+    allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True)
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0  # could not measure, so guess
+
+# Initialize components for Routine "instructions"
+instructionsClock = core.Clock()
+instText = visual.TextStim(win=win, name='instText',
+    text='What is your user code?\n',
+    font='Arial',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0);
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+target = visual.TextStim(win=win, name='target',
+    text='default text',
+    font='Arial',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0);
+sound_1 = sound.Sound('A', secs=-1)
+sound_1.setVolume(1)
+image = visual.ImageStim(
+    win=win, name='image',units='pix', 
+    image='robot_imgs/pepper', mask=None,
+    ori=0, pos=(0, 0), size=(768/3,950/3),
+    color=[1,1,1], colorSpace='rgb', opacity=1,
+    flipHoriz=False, flipVert=False,
+    texRes=128, interpolate=True, depth=-3.0)
+image_2 = visual.ImageStim(
+    win=win, name='image_2',units='pix', 
+    image='robot_imgs/pr2', mask=None,
+    ori=0, pos=(-50-(768/3),9, 0), size=(768/3,950/3),
+    color=[1,1,1], colorSpace='rgb', opacity=1,
+    flipHoriz=False, flipVert=False,
+    texRes=128, interpolate=True, depth=-4.0)
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+# ------Prepare to start Routine "instructions"-------
+t = 0
+instructionsClock.reset()  # clock
+frameN = -1
+continueRoutine = True
+# update component parameters for each repeat
+key_resp_3 = event.BuilderKeyResponse()
+# keep track of which components have finished
+instructionsComponents = [instText, key_resp_3]
+for thisComponent in instructionsComponents:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+
+# -------Start Routine "instructions"-------
+while continueRoutine:
+    # get current time
+    t = instructionsClock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *instText* updates
+    if t >= 0.0 and instText.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        instText.tStart = t
+        instText.frameNStart = frameN  # exact frame index
+        instText.setAutoDraw(True)
+    
+    # *key_resp_3* updates
+    if t >= 0.0 and key_resp_3.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        key_resp_3.tStart = t
+        key_resp_3.frameNStart = frameN  # exact frame index
+        key_resp_3.status = STARTED
+        # keyboard checking is just starting
+        win.callOnFlip(key_resp_3.clock.reset)  # t=0 on next screen flip
+        event.clearEvents(eventType='keyboard')
+    if key_resp_3.status == STARTED:
+        theseKeys = event.getKeys()
+        
+        # check for quit:
+        if "escape" in theseKeys:
+            endExpNow = True
+        if len(theseKeys) > 0:  # at least one key was pressed
+            key_resp_3.keys = theseKeys[-1]  # just the last key pressed
+            key_resp_3.rt = key_resp_3.clock.getTime()
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in instructionsComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # check for quit (the Esc key)
+    if endExpNow or event.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "instructions"-------
+for thisComponent in instructionsComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# check responses
+if key_resp_3.keys in ['', [], None]:  # No response was made
+    key_resp_3.keys=None
+thisExp.addData('key_resp_3.keys',key_resp_3.keys)
+if key_resp_3.keys != None:  # we had a response
+    thisExp.addData('key_resp_3.rt', key_resp_3.rt)
+thisExp.nextEntry()
+# the Routine "instructions" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# set up handler to look after randomisation of conditions etc
+trials = data.TrialHandler(nReps=1, method='random', 
+    extraInfo=expInfo, originPath=-1,
+    trialList=data.importConditions('sounds.xlsx'),
+    seed=None, name='trials')
+thisExp.addLoop(trials)  # add the loop to the experiment
+thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
+# abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+if thisTrial != None:
+    for paramName in thisTrial:
+        exec('{} = thisTrial[paramName]'.format(paramName))
+
+for thisTrial in trials:
+    currentLoop = trials
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+    if thisTrial != None:
+        for paramName in thisTrial:
+            exec('{} = thisTrial[paramName]'.format(paramName))
+    
+    # ------Prepare to start Routine "trial"-------
+    t = 0
+    trialClock.reset()  # clock
+    frameN = -1
+    continueRoutine = True
+    # update component parameters for each repeat
+    target.setText(task_2
+
+)
+    response = event.BuilderKeyResponse()
+    sound_1.setSound(task_2, secs=6)
+    # keep track of which components have finished
+    trialComponents = [target, response, sound_1, image, image_2]
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    
+    # -------Start Routine "trial"-------
+    while continueRoutine:
+        # get current time
+        t = trialClock.getTime()
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *target* updates
+        if t >= 0.5 and target.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            target.tStart = t
+            target.frameNStart = frameN  # exact frame index
+            target.setAutoDraw(True)
+        
+        # *response* updates
+        if t >= 0.5 and response.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            response.tStart = t
+            response.frameNStart = frameN  # exact frame index
+            response.status = STARTED
+            # keyboard checking is just starting
+            win.callOnFlip(response.clock.reset)  # t=0 on next screen flip
+            event.clearEvents(eventType='keyboard')
+        if response.status == STARTED:
+            theseKeys = event.getKeys(keyList=['left', 'right', 'down'])
+            
+            # check for quit:
+            if "escape" in theseKeys:
+                endExpNow = True
+            if len(theseKeys) > 0:  # at least one key was pressed
+                response.keys = theseKeys[-1]  # just the last key pressed
+                response.rt = response.clock.getTime()
+                # a response ends the routine
+                continueRoutine = False
+        # start/stop sound_1
+        if t >= 0.0 and sound_1.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            sound_1.tStart = t
+            sound_1.frameNStart = frameN  # exact frame index
+            sound_1.play()  # start the sound (it finishes automatically)
+        frameRemains = 0.0 + 6- win.monitorFramePeriod * 0.75  # most of one frame period left
+        if sound_1.status == STARTED and t >= frameRemains:
+            sound_1.stop()  # stop the sound (if longer than duration)
+        
+        # *image* updates
+        if t >= 0.0 and image.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            image.tStart = t
+            image.frameNStart = frameN  # exact frame index
+            image.setAutoDraw(True)
+        
+        # *image_2* updates
+        if t >= 0.0 and image_2.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            image_2.tStart = t
+            image_2.frameNStart = frameN  # exact frame index
+            image_2.setAutoDraw(True)
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in trialComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # check for quit (the Esc key)
+        if endExpNow or event.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # -------Ending Routine "trial"-------
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    # check responses
+    if response.keys in ['', [], None]:  # No response was made
+        response.keys=None
+    trials.addData('response.keys',response.keys)
+    if response.keys != None:  # we had a response
+        trials.addData('response.rt', response.rt)
+    sound_1.stop()  # ensure sound has stopped at end of routine
+    # the Routine "trial" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset()
+    thisExp.nextEntry()
+    
+# completed 1 repeats of 'trials'
+
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsWideText(filename+'.csv')
+thisExp.saveAsPickle(filename)
+logging.flush()
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
+core.quit()
---
+++ _robotVoice.py
@@ -0,0 +1,108 @@
+#import modules
+from psychopy import core, visual, gui, data, event, sound
+from psychopy.tools.filetools import fromFile, toFile
+import numpy, random
+
+# STEP 02: SAVE A BUNCH OF PARAMS
+#########
+try:  
+    expInfo = fromFile('lastParams.pickle')
+except:  
+    # create dictionary of terms that are needed
+    expInfo = {
+    "robot_1":"robot_imgs/stevie.png",
+    "robot_2":"robot_imgs/pr2.png",
+    "robot_3":"robot_imgs/pepper.png"
+    }
+    toFile('lastParams.pickle', expInfo)
+expInfo['dateStr'] = data.getDateStr()  # add the current time
+
+# optional: open a dialog box to get input or change params - see t2.py
+'''
+gui = gui.Dlg()
+gui.addField("Subject ID:")
+gui.addField("Condition Number:")
+gui.show()
+print(gui.data)
+subj_id = gui.data[0]
+cond_num = int(gui.data[1])
+'''
+
+# STEP 03: CREATE A WINDOW AND CLOCK
+#########
+win = visual.Window(
+    size=[1440, 810], 
+    fullscr=True, 
+    screen=0,
+    units='pix'
+    )
+
+clock = core.Clock()
+mouse = event.Mouse(visible=True,newPos=False,win=win)
+
+# STEP 04: PLAY SOUND 
+voice = sound.Sound('sounds/pepper_sydney/sentence_02.wav')
+waitTime = voice.getDuration()
+listenText = visual.TextStim(win,pos=[0,450],text="Listen to the sound",height=40,wrapWidth=1000,units='pix')
+listenText.draw()
+win.flip()
+voice.play()
+core.wait(waitTime)
+
+# STEP 04: PLOT IMAGES ON SCREEN
+#########
+while not event.getKeys(keyList=["e"]):
+    #mouse= event.Mouse()
+
+    img_robo1 = visual.ImageStim(win=win,
+        image=expInfo["robot_1"],
+        ori=0, 
+        pos=(0, 150), 
+        size=(768/2.5,950/2.5),
+        units='pix'
+        )
+    img_robo2 = visual.ImageStim(win=win,
+        image=expInfo["robot_2"],
+        ori=0, 
+        pos=(0, -300), 
+        size=(768/2.5,950/2.5),
+        units='pix'
+        )
+
+    img_robo1.draw()
+    img_robo2.draw()
+
+    # STEP 05: ADD TEXT TO THE SCREEN
+    #########
+    message1 = visual.TextStim(win, 
+        pos=[0,450],
+        text="What robot best suits the voice?",
+        height=40,
+        wrapWidth=1000,
+        units='pix'
+        )
+    message1.draw()
+
+    win.flip()
+
+    # STEP 06: CAPTURE WHICH IMAGE WAS CLICKED
+    # option 1: use keys
+    #keys = event.waitKeys(keyList=["1","2","3","4","5","6","7","8"],timeStamped=clock) 
+    #print(keys)
+
+    # option 2: mouse press
+    print(img_robo1.contains(mouse))
+    if sum(mouse.getPressed()) and img_robo1.contains(mouse):
+        box4 = visual.Rect(win=win,lineColor="black",fillColor="LightBlue",size=[100,50],pos=[-200,-90])
+        box4.draw()
+        win.flip()
+        print("I love fat titties")
+
+#if sum(mouse.getPressed()) and myShape.contains(mouse):
+#    print("My shape was pressed")
+##############
+
+event.waitKeys()  # wait for participant to respond
+
+win.close()
+core.quit()
